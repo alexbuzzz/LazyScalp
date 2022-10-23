0f4e5b413bcc31f413c2d330acd34501f999353b
AlexBuz <sasha.buzik@gmail.com>
2022-10-23 11:27:41 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+// Change theme theme
+// document.documentElement.setAttribute('data-theme', 'white')
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/charts">Charts</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
